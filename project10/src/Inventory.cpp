303e644a72989508dd3214a985d55c711d804b26
Arpit Bajpai <arpitbajpai038@gmail.com>
2026-08-09 22:56:42 +0530

    Update comment in Inventory.cpp

--- project10/src/Inventory.cpp
+++ project10/src/Inventory.cpp
@@ -1,6 +1,6 @@
 #include "Inventory.h"
 #include <iostream>
-//......4
+//......4...........hjhjhjhjhjhjuujjj
 Inventory::Inventory(int id, int quantity, int reorderLevel, int reorderQuantity)
     : productId(id), quantityOnHand(quantity), 
       reorderLevel(reorderLevel), reorderQuantity(reorderQuantity) {}
