2d8cb3f24006d65d282383b0551c572913425146
Arpit Bajpai <arpitbajpai038@gmail.com>
2026-08-14 21:38:45 +0530

    Remove commented out code in Inventory.cpp

--- project10/src/Inventory.cpp
+++ project10/src/Inventory.cpp
@@ -1,6 +1,6 @@
 #include "Inventory.h"
 #include <iostream>
-//.....l...............///.............////
+//////.....
 Inventory::Inventory(int id, int quantity, int reorderLevel, int reorderQuantity)
     : productId(id), quantityOnHand(quantity), 
       reorderLevel(reorderLevel), reorderQuantity(reorderQuantity) {}
